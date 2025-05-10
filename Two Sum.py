65a8940d8c09402affc4e7f8695470f58b9914e3
farnazradaei <farnaz.radaei@gmail.com>
2025-05-10 23:47:45 +0330

    Two sum

---
+++ Two Sum.py
@@ -0,0 +1,19 @@
+"""
+Project description:
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+"""
+
+nums=[3,2,4]
+target= 6
+
+for i in range(len(nums)) :
+    for j in range (i+1 , len(nums)):
+        if nums[i] + nums[j] == target:
+            print([i,j])
+            break
+
