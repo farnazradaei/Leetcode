fcea84e47f00c8dfce70686d135fcafbd39974ce
farnazradaei <farnaz.radaei@gmail.com>
2025-09-20 15:16:54 +0330

    first solu

---
+++ Count Primes.py
@@ -0,0 +1,27 @@
+# Given an integer n, return the number of prime numbers that are strictly less than n.
+
+# Example 1:
+
+# Input: n = 10
+# Output: 4
+# Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+# Example 2:
+
+# Input: n = 0
+# Output: 0
+# Example 3:
+
+# Input: n = 1
+# Output: 0
+
+class Solution(object):
+    def is_prime(self, n):
+        if n < 2:
+            return False
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+    
+    def countPrimes(self, n):
+        return len([i for i in range(2, n) if self.is_prime(i)])
