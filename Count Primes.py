45ce1969e64ffecf1d07ff29fb6313b7b0d931df
farnazradaei <farnaz.radaei@gmail.com>
2025-09-20 15:18:23 +0330

    Sieve of Eratosthenes

--- Count Primes.py
+++ Count Primes.py
@@ -15,13 +15,16 @@
 # Output: 0
 
 class Solution(object):
-    def is_prime(self, n):
-        if n < 2:
-            return False
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
-    
     def countPrimes(self, n):
-        return len([i for i in range(2, n) if self.is_prime(i)])
+        if n < 2:
+            return 0
+        
+        is_prime = [True] * n
+        is_prime[0] = is_prime[1] = False  
+        
+        for i in range(2, int(n**0.5) + 1):
+            if is_prime[i]:
+                for j in range(i*i, n, i):
+                    is_prime[j] = False
+        
+        return sum(is_prime)
