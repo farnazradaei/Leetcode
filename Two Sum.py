086eecda33ee4f6b49be52ccf97112638bfed017
farnazradaei <farnaz.radaei@gmail.com>
2025-10-15 17:56:57 +0330

    HashMap solution

--- Two Sum.py
+++ Two Sum.py
@@ -1,22 +1,16 @@
-"""
-Project description:
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+class Solution(object):
+    def twoseen(self, nums, target):
+        seen = {}
+        for i , num in enumerate(nums):
+           complement = target - num  
+           if complement in seen :
+               return [seen[complement] , i]
+           
+           else:
+               seen[num] = i 
+               
+            
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-You can return the answer in any order.
-
-"""
-
-nums=[3,2,4]
-target= 6
-found = False
-for i in range(len(nums)) :
-    for j in range (i+1 , len(nums)):
-        if nums[i] + nums[j] == target:
-            print([i,j])
-            found= True
-            break
-    if found:
-        break    
-
+sol = Solution()
+print(sol.twoseen(nums = [3,3], target = 6))
