bed471bf143ab9456e28d6b0592785d9ccbbc41f
farnazradaei <farnaz.radaei@gmail.com>
2025-10-15 16:13:59 +0330

    binary search

--- Search Insert Position.py
+++ Search Insert Position.py
@@ -1,13 +1,23 @@
 class Solution(object):
     def searchInsert(self, nums, target):
-        for i, num in enumerate(nums):
-            if target == num:
-                return i
-            elif target < num :
-                return i
-            
-        return len(nums)
+        left = 0 
+        right = len(nums) - 1
 
+        while left <= right :
+            mid = (left + right) // 2 
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target : 
+                left = mid + 1 
+            else:
+                right = mid - 1 
 
+        return left
+    
 sol = Solution()
-print(sol.searchInsert(nums = [1,3,5,6], target = 5))
+print(sol.searchInsert(nums = [1,3,4,5,6,7] , target = 2))
+
+
+                
+
+
