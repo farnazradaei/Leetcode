da926199142eea271496d3fd2d0965e2122f5a3e
farnazradaei <farnaz.radaei@gmail.com>
2025-10-16 13:55:25 +0330

    HashMap solution

--- Intersection of Two Arrays.py
+++ Intersection of Two Arrays.py
@@ -3,17 +3,25 @@ class Solution(object):
         moshtrk = {}
         aaza = []
         for num in nums1 : 
-            moshtrk [num] = nums1 
-        for num in nums2 : 
             if num in moshtrk:
-                if num not in aaza:
-                    aaza.append(num)
-                    
-            
+                 moshtrk[num] += 1
+            else : 
+                 moshtrk[num] = 1 
+        for num in nums2 : 
+            if num in moshtrk and moshtrk[num] > 0 :
+                aaza.append(num)
+                moshtrk[num] -= 1
+
         return aaza
 
+                   
+        
+                
             
+         
 
+            
+#if num not in aaza:
 
 sol = Solution()
-print(sol.intersection( nums1 = [1,2,2,1], nums2 = [2,2]))
+print(sol.intersection(nums1 = [4,9,5], nums2 = [9,4,9,8,4]))
